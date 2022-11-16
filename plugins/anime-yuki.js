32798d02323869715d94b1967ee7950617596774
Akamaru69 <118302996+Akamaru69@users.noreply.github.com>
2022-11-17 02:25:25 +0700

    Create anime-yuki.js

---
+++ plugins/anime-yuki.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://raw.githubusercontent.com/Akamaru69/Databasee/main/Random%20Anime/yuki.json'
+	conn.sendButton(m.chat, 'Wangy wangy (≧ω≦)', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+}
+handler.command = /^(yuki)$/i
+handler.tags = ['anime']
+handler.help = ['yuki']
+handler.premium = false
+
+export default handler
