b43362a7b9aea782ba46695372a8964bc7bd4ad2
Akamaru69 <118302996+Akamaru69@users.noreply.github.com>
2022-11-18 06:53:03 +0700

    Update anime-awoo.js

--- plugins/anime-awoo.js
+++ plugins/anime-awoo.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, command }) => {
 	let url = 'https://api.lolhuman.xyz/api/random/awoo?apikey=3c508dcc8d907cdc15404b86'
 		conn.sendButton(m.chat, 'Wibu stress', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 		}
-		handler.command = /^(awoi)$/i
+		handler.command = /^(awoo)$/i
 		handler.tags = ['anime']
 		handler.help = ['awoo']
 		handler.limit = true
