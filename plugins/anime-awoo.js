7411fb8988678dd1ed90a8786110fff91ff13ed7
Akamaru69 <118302996+Akamaru69@users.noreply.github.com>
2022-11-18 01:24:41 +0700

    Create anime-awoo.js

---
+++ plugins/anime-awoo.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://api.lolhuman.xyz/api/random/awoo?apikey=3c508dcc8d907cdc15404b86'
+		conn.sendButton(m.chat, 'Wibu stress', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+		}
+		handler.command = /^(awoi)$/i
+		handler.tags = ['anime']
+		handler.help = ['awoo']
+		handler.limit = true
+export default handler
