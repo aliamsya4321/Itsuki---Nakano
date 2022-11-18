30a9cc494705a3affb4ec3d6e7c0d5fbc3a5821e
Akamaru69 <118302996+Akamaru69@users.noreply.github.com>
2022-11-18 07:16:30 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,4 +1,5 @@
-let handler =  m => m.reply(`
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
 ╭─「 🧚🏻‍♂️ *DONASI* 」
 │
 ├ PULSA / DANA / GOPAY :
@@ -11,7 +12,23 @@ let handler =  m => m.reply(`
 ├ wa.me/6282123019956
 ╰───────────
 © 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮 𝐅𝐢𝐤𝐫𝐢𝐢
-`.trim()) // Tambah sendiri kalo mau
+`
+
+const templateButtons = [
+    {index: 1, urlButton: {displayText: '✨ Saweria', url: psaweria}},
+    {index: 2, urlButton: {displayText: '📷 Instagram', url: sig}},
+    {index: 3, urlButton: {displayText: '🌎 Official Group', url: sgc}},
+    {index: 4, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
+    {index: 5, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
+}
+conn.sendMessage(m.chat, tm, m)
+}
 
 handler.menugroup = ['donasi']
 handler.tagsgroup = ['group']
